db30ed3f5b688ad637c07ede084c26457d94ea1a
Ian Mazzaglia <ianmazzaglia@gmail.com>
2017-01-25 20:16:39 -0300

    baja cliente

--- controladores/controlador_baja_cliente.py
+++ controladores/controlador_baja_cliente.py
@@ -2,13 +2,22 @@
 Esta clase representa el controlador de la vista form_alta_cliente
 """
 
-from PyQt5.QtWidgets import QApplication, QDialog, QMessageBox
+from PyQt5.QtWidgets import QApplication, QDialog, QMessageBox, QWidget, QPushButton
 from vistas_py.form_baja_cliente import Ui_BajaCLiente
 from modelo.modelo import *
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 class ControladorBajaCliente(QDialog):
+
+    def __init__(self):
+        super().__init__()
+        self.title = 'PyQt5 messagebox - pythonspot.com'
+        self.left = 10
+        self.top = 10
+        self.width = 320
+        self.height = 200
+        self.initUI()
 
     def __init__(self):
         super(ControladorBajaCliente, self).__init__()
@@ -27,21 +36,25 @@ class ControladorBajaCliente(QDialog):
         Session = sessionmaker(bind=engine)
         session = Session()
 
-        if self.form_alta_cliente.text_nombre.text() == "" or self.form_alta_cliente.text_apellido.text() == "":
+        if self.form_baja_cliente.text_nombre.text() == "" or self.form_baja_cliente.text_apellido.text() == "":
             QMessageBox.information(self, "Campos incompletos", "Debe completar todos los campos.")
         else:
-            nombre = str(self.form_alta_cliente.text_nombre.text())
-            apellido = str(self.form_alta_cliente.text_apellido.text())
+            nombre = str(self.form_baja_cliente.text_nombre.text())
+            apellido = str(self.form_baja_cliente.text_apellido.text())
 
             cliente_base = session.query(Cliente).filter(Cliente.nombre == nombre, Cliente.apellido == apellido).first()
             print(cliente_base)
 
-            if cliente_base == None:
-                cliente = Cliente(nombre, apellido)
-                session.add(cliente)
-                session.commit()
-                self.close()
+            if cliente_base != None:
+                buttonReply = QMessageBox.question(self, 'Mensaje', "¿esta seguro que desea elimnar al cliente?",
+                                                   QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
+                if buttonReply == QMessageBox.Yes:
+                    session.delete(cliente_base)
+                    session.commit()
+                    self.close()
+                else:
+                    self.close()
             else:
                 print("El cliente existe")
                 QMessageBox.information(self, "Error",
-                                        "El cliente ya existe, intente darlo de alta nuevamente.")
+                                        "El cliente no existe, intente ingresarlo nuevamente.")
--- controladores/main.py
+++ controladores/main.py
@@ -6,6 +6,7 @@ from vistas_py.menuprincipal import Ui_menuprincipal
 from controladores.controlador_alta_cliente import *
 from controladores.controlador_alta_servicio import *
 from controladores.controlador_nuevo_turno import *
+from controladores.controlador_baja_cliente import *
 from controladores.controlador_listado_clientes import *
 
 class ControladorMenuPrincipal(QMainWindow):
@@ -17,6 +18,7 @@ class ControladorMenuPrincipal(QMainWindow):
         self.menu_principal.setupUi(self)
 
         self.menu_principal.action_alta_Cliente.triggered.connect(self.alta_cliente)
+        self.menu_principal.action_baja_Cliente.triggered.connect(self.baja_cliente)
         self.menu_principal.action_alta_servicio.triggered.connect(self.alta_servicio)
         self.menu_principal.action_nuevo_turno.triggered.connect(self.alta_turno)
         self.menu_principal.action_listado_cliente.triggered.connect(self.listar_clientes)
@@ -25,6 +27,11 @@ class ControladorMenuPrincipal(QMainWindow):
         print("alta cliente")
         ac = ControladorAltaCliente()
         ac.exec_()
+
+    def baja_cliente(self):
+        print("alta cliente")
+        bc = ControladorBajaCliente()
+        bc.exec_()
 
     def listar_clientes(self):
         print("listar clientes")
